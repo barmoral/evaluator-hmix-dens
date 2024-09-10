b13d8aeb398600c0e06bf7daa585e1559f6ff450
Barbara Morales <bamo6610@colorado.edu>
2024-09-10 11:02:58 -0600

    results for box size test - mixture with 250 total molecules

---
+++ test_250_tip3pfb/run_eval_hmix_dens.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=30:00:00
+#SBATCH --partition=blanca-shirts
+#SBATCH --qos=blanca-shirts
+#SBATCH --account=blanca-shirts
+#SBATCH --gres=gpu
+#SBATCH --job-name=tip3pfb_250
+#SBATCH --output=slurm_codes/hmix_dens_250.%j.log
+
+module purge
+module avail
+ml anaconda
+conda activate evaluator-blanca
+
+python hmix_dens_calc.py
+echo "Hmix+Density test for OCCN(CCO)CCO in TIP3P-FB in 250 total molecules system size"
+
+sacct --format=jobid,jobname,cputime,elapsed
