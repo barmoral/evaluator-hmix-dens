5354fb4ae694fb7e7de8f32c25457f24e3b0ad0d
Barbara Morales <bamo6610@colorado.edu>
2024-09-27 11:18:59 -0600

    added test for 1000 molecules and 20 ns replicate 4

---
+++ test_1000_longer_r4/run_eval_hmix_dens.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=60:00:00
+#SBATCH --partition=blanca-shirts
+#SBATCH --qos=blanca-shirts
+#SBATCH --account=blanca-shirts
+#SBATCH --gres=gpu
+#SBATCH --job-name=500_longer_r4
+#SBATCH --output=slurm_codes/500_longer_r4.%j.log
+
+module purge
+module avail
+ml anaconda
+conda activate evaluator-blanca
+
+python setup-options.py
+python hmix_dens_calc.py
+echo "Hmix+Density test for OCCN(CCO)CCO in TIP3P-FB in 500 total molecules system size. Longer run. r4"
+
+sacct --format=jobid,jobname,cputime,elapsed
